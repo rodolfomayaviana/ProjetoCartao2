832c473a0e9566f6068fc5c038c8a35f1bd0a760
rodolfomayaviana <rodolfo.mayaviana@hotmail.com>
2017-05-31 17:03:00 -0700

    deploy heroku

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,6 +21,7 @@ class DefaultController extends Controller
     public function inicioAction(Request $request)
     {
 
+
 	$gastoController = new GastoController($this->getDoctrine()->getEntityManager(), $this->container);
 	$gastoes = $gastoController->getGastoes(2016, 01);
         return $this->render('inicio.html.twig');
